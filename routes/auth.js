8372dda4e72ae476961cc2bbf20ba33f9c7e0036
Amit Patil <amitgpatil215@gmail.com>
2021-09-12 15:38:24 +0530

    added register and login validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const userModel = require("../model/User");
+const {registerValidation}=require('../validators/validation');
 
 router.post("/register", async (req, res) => {
+  // Lets Validate User before saving to DB
+  const {error} =registerValidation(req.body);
+  if (error) return res.status(400).send(error?.details[0]?.message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
@@ -10,11 +15,10 @@ router.post("/register", async (req, res) => {
   });
 
   try {
-    const savedUser=await user.save();
+    const savedUser = await user.save();
     res.send(savedUser);
-
   } catch (err) {
-      res.status(400).send(err);
+    res.status(400).send(err);
   }
 });
 
