1b7b98ee6ae689b7b8fec15376ae7e581f629c99
Amit Patil <amitgpatil215@gmail.com>
2021-09-13 17:25:34 +0530

    made ready for deployment

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ app.use(express.json());
 app.use("/api/user", authRoute);
 app.use("/api/", postRoute);
 
-app.listen(3000, () => console.log("Server up and running"));
+app.listen(process.env.PORT || 3000, () => console.log("Server up and running"));
